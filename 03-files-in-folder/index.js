6a361b2af622cf8b4516230f9d5315145240313e
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-11-04 12:25:07 +0300

    feat: implement 3 task. last version i hope

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,11 +1,14 @@
-const { SSL_OP_COOKIE_EXCHANGE } = require('constants');
 const fs = require('fs');
 const path = require('path');
 fs.readdir(path.join(__dirname,'/secret-folder'), { withFileTypes: true }, (err, files) => {
   if (err) throw err;
-  for(let i = 0; i < files.length;i++){
-    console.log(files[i]);
-    console.log(files[i].isFile());
-    console.log((path.extname(`${files[i].name}`)).slice(1));
-  }
-})
+  for(let i = 0; i < files.length; i++){
+    if(files[i].isFile()) {
+     //console.log((path.parse(files[i]['name'])['name']) + ' - ' + path.parse(files[i]['name'])['ext'].slice(1));
+     path.parse(files[i]['name'])['name'] + ' - ' + path.parse(files[i]['name'])['ext'].slice(1);
+     fs.stat(path.join(__dirname, 'secret-folder', files[i]['name']), (err, data) => {
+        if (err) throw err;
+        console.log((path.parse(files[i]['name'])['name']) + ' - ' + path.parse(files[i]['name'])['ext'].slice(1) + ' - ' + data.size);
+      } )   
+    }
+}})
