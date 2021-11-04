2b5b8f157d980b82d4dd3ce195e2daa78df9a95e
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-11-04 14:23:16 +0300

    feat: implement 2 task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,23 +1,26 @@
 const process = require('process');
 const fs = require('fs');
 const path = require('path');
-const stream = new fs.WriteStream(path.join(__dirname, '/write.txt'));
-stream.write('kjhhgfgdgrsfeaeASDKU12666');
-const readline = require('readline').createInterface({
-input: process.stdin,
-output: process.output
-})
+const readline = require('readline');
 const { stdin, stdout } = process;
-stdout.write('Как тебя зовут?\n');
-stdin.on('data', data => {
-  stdout.write('Привет, ');
-  stdout.write(data);
+const stream = new fs.WriteStream(path.join(__dirname, 'write.txt'));
+  readline.createInterface({
+    input: process.stdin,
+    output: process.output
+});
+  stdout.write('Как вас зовут?\n');
+  stdin.on('data', data => {
+    if(data.toString().trim() === 'exit'){
+    process.exit();
+  } else {
+    stdout.write('Привет, ');
+    stdout.write(data);
+    stream.write(`${data}`)
+  }
+});
+  process.on('SIGINT', () => {
   process.exit();
 });
-process.on('exit', code => {
-  if (code === 0) {
-      stdout.write('Всё в порядке');
-  } else {
-      stderr.write(`Что-то пошло не так. Программа завершилась с кодом ${code}`);
-  }
+  process.on('exit', () => {
+  console.log('До свидания');
 });
