efb3fd8de4ae40c209baff6b9d95b776b994b8ec
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-11-08 19:23:33 +0300

    feat: implement 6 task last version

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,14 +3,22 @@ const path = require('path');
 const sourceTemplate = path.join(__dirname, 'template.html');
 const dest = path.join(__dirname, 'project-dist', 'index.html');
 const components = path.join(__dirname, 'components');
+const fspromise = require('fs').promises;
 
-function createDir() {
-  fs.mkdir('06-build-page/project-dist', {
+async function deleteDir(){
+  await fspromise.rm('06-build-page/project-dist', {
+    recursive: true, force: true
+  }, (err) => { if (err) throw err;
+  })
+}
+
+async function createDir() {
+ await fspromise.mkdir('06-build-page/project-dist', {
     recursive: true
-  }, (err) => { //создаем папку project-dist
+  }, (err) => {
     if (err) console.log(`Error creating directory: ${err}`)
   })
-  fs.copyFile(sourceTemplate, dest, function (err) {
+  await fs.copyFile(sourceTemplate, dest, function (err) {
     if (err) throw err;
   })
 }
@@ -41,20 +49,12 @@ async function replaceComponents() {
     })
   })
 }
-async function build() {
-  createDir();
-  await replaceComponents();
-}
-build();
-
-
-const sourceFolder = path.join(__dirname,'styles');
-
+async function createScc(){
+  const sourceFolder = path.join(__dirname,'styles');
   fs.readdir(path.join(sourceFolder), { withFileTypes: true }, (err, files) => { //чтение содержимого папки styles
     if (err) throw err;
     for(let i = 0; i < files.length; i++){
       if(files[i].isFile() && (path.parse(files[i]['name'])['ext']) === '.css') {  //проверка на файл и на расширение
-
         const stream = new fs.ReadStream(path.join(sourceFolder + '/' + files[i]['name']), 'utf8'); //чтение файлов css
         stream.on('data', function(data){
           const streamWr = new fs.appendFile((path.join(__dirname, 'project-dist', 'style.css')), data, (err) => {//запись
@@ -64,3 +64,26 @@ const sourceFolder = path.join(__dirname,'styles');
       }
     }
   })
+}
+
+async function copyDir(srcAssets, destAssets) {
+ const entries = await fspromise.readdir(srcAssets, {withFileTypes: true});
+await fspromise.mkdir(destAssets);
+for(let entry of entries) {
+  const srcPath = path.join(srcAssets, entry.name);
+  const destPath = path.join(destAssets, entry.name);
+  if (entry.isDirectory()) {
+    await copyDir(srcPath, destPath);
+  } else {
+    await fspromise.copyFile(srcPath, destPath);
+  }
+}
+}
+async function build() {
+  await deleteDir();
+  await createDir();
+  await replaceComponents();
+  await copyDir(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
+  await createScc();
+}
+build();
