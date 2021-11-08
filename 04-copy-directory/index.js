db5a2262dc5dd47170f9521f5480e00e424b3a2b
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-11-08 20:16:50 +0300

    feat: add rm function in 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,43 @@
 const fs = require('fs');
 const path = require('path');
-const sourceFolder = path.join(__dirname,'files');
-const destinationFolder = path.join(__dirname,'files-copy');
+const sourceFolder = path.join(__dirname, 'files');
+const destinationFolder = path.join(__dirname, 'files-copy');
+const fspromise = require('fs').promises;
 
-  fs.mkdir('04-copy-directory/files-copy', {recursive:true}, (err)=>{
-    if (err) console.log(`Error creating directory: ${err}`)
+async function deleteDir() {
+  await fspromise.rm('04-copy-directory/files-copy', {
+      recursive: true,
+      force: true
+    },
+    (err) => {
+      if (err) throw err;
+    });
+};
+async function createDir() {
+  await fspromise.mkdir('04-copy-directory/files-copy', {
+    recursive: true
+  }, (err) => {
+    if (err) console.log(`Error creating directory: ${err}`);
   })
-  fs.readdir(path.join(sourceFolder), { withFileTypes: true }, (err, files) => {
+}
+
+async function readDir() {
+  fs.readdir(path.join(sourceFolder), {
+    withFileTypes: true
+  }, (err, files) => {
     if (err) throw err;
-    for(let i = 0; i < files.length; i++){
-      if(files[i].isFile()) {
-        fs.copyFile(sourceFolder + '/' + files[i]['name'], destinationFolder + '/' +files[i]['name'], function(err) {
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].isFile()) {
+        fs.copyFile(sourceFolder + '/' + files[i]['name'], destinationFolder + '/' + files[i]['name'], function (err) {
           if (err) throw err;
-// console.log(files[i]['name'])
         })
       }
-  }})
+    }
+  })
+}
+async function copyedFile() {
+  await deleteDir();
+  await createDir();
+  await readDir();
+}
+copyedFile();
